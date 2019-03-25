0379e72d92356a56666b00e1a0e3304f75842334
Rachel Harris <rharri63@vols.utk.edu>
2019-03-25 13:05:49 -0400

    Clarified some doubts

--- tracker.ino
+++ tracker.ino
@@ -3,4 +3,4 @@
 //Author: Rachel Harris
 //Purpose: Get the robot to remember its path and store its path in something
 
-
+//This may or may not actually be possible; there's no arduino library for it, and I don't know if I'm that good...
